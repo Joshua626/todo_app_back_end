3f9967028cca0ec9a021c6a7c1ce1b4053d46daa
Joshua Villanueva <joshuavillanueva2688@gmail.com>
2017-07-20 14:09:22 +0000

    updated

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,6 +3,18 @@ class TodoController < ApplicationController
     end
     
     def show
-        @todo_description= "Take out the trash"
+        todo_id= '1'
+        if todo_id= '1'
+             @todo_description= "Do laundry"
+        elsif todo_id == '2'       
+             @todo_description= "Take out the trash"
+        elsif todo_id== '3'
+             @todo_description= "cut the grass"
+        elsif todo_id== '4'
+             @todo_description= "clean my room"
+        elsif todo_id== '5'
+             @todo_description= "do dishes"
+        end
     end
+    
 end
