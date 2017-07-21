601f40570b764fa32302f0f99fe2298f668f9c96
Joshua Villanueva <joshuavillanueva2688@gmail.com>
2017-07-21 12:24:18 +0000

    add the edit

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -8,5 +8,23 @@ class TodoController < ApplicationController
     def new
     end
     def create
+        t = Todo.new
+        t.description = ['description']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
+    def edit
+        @todo = Todo.find_by_id(params[:id]) 
+    end
+    def update
+        t = Todo.find_by_id(params['id'])
+        t.description = ['description']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
+    def destroy
+        t = Todo.find_by_id(params[:id])
+        t.destroy
+        redirect_to "/todo/index"
     end
 end
